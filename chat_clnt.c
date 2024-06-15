edfed081fcdde157a6bbf2d62e729406f81c3f30
MA_YUHAN <myh1290188694@gmail.com>
2024-06-16 02:13:58 +0900

    Socket Creation and Connection:
    socket(PF_INET, SOCK_STREAM, 0): Creates a TCP socket.
    connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)): Connects the socket to the server specified by IP and port.
    Threads for Sending and Receiving Messages:
    
    pthread_create(&snd_thread, NULL, send_msg, (void *) &sock): Creates a thread for sending messages.
    pthread_create(&rcv_thread, NULL, recv_msg, (void *) &sock): Creates a thread for receiving messages.
    pthread_join(snd_thread, &thread_return): Waits for the send thread to finish.
    pthread_join(rcv_thread, &thread_return): Waits for the receive thread to finish.
    Sending Messages:
    
    fgets(msg, BUF_SIZE, stdin): Reads a message from the user.
    Special commands (e.g., /invite, /yes, /no, /out, /id) are sent directly.
    Other messages are prefixed with the username and then sent.
    Receiving Messages:
    
    read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1): Reads messages from the server.
    Messages are printed to the terminal.
    Error Handling:
    
    error_handling(char * msg): Prints an error message and exits the program.

--- chat_clnt.c
+++ chat_clnt.c
@@ -1,88 +1,87 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
-	
+
 #define BUF_SIZE 100
 #define NAME_SIZE 20
-	
+
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
-	
-char name[NAME_SIZE]="[DEFAULT]";
+
+char name[NAME_SIZE] = "[DEFAULT]";
 char msg[BUF_SIZE];
-	
-int main(int argc, char *argv[])
-{
-	int sock;
-	struct sockaddr_in serv_addr;
-	pthread_t snd_thread, rcv_thread;
-	void * thread_return;
-	if(argc!=4) {
-		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
-		exit(1);
-	 }
-	
-	sprintf(name, "[%s]", argv[3]);
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-	  
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-		error_handling("connect() error");
-	
-	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
-	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
-	pthread_join(snd_thread, &thread_return);
-	pthread_join(rcv_thread, &thread_return);
-	close(sock);  
-	return 0;
+
+int main(int argc, char *argv[]) {
+    int sock;
+    struct sockaddr_in serv_addr;
+    pthread_t snd_thread, rcv_thread;
+    void * thread_return;
+
+    if (argc != 4) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+        exit(1);
+    }
+
+    sprintf(name, "[%s]", argv[3]); // Set username
+    sock = socket(PF_INET, SOCK_STREAM, 0); // Create a socket
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]); // Set server IP address
+    serv_addr.sin_port = htons(atoi(argv[2])); // Set port number
+
+    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("connect() error"); // Connect to the server
+    }
+
+    // Create threads for sending and receiving messages
+    pthread_create(&snd_thread, NULL, send_msg, (void *) &sock);
+    pthread_create(&rcv_thread, NULL, recv_msg, (void *) &sock);
+    pthread_join(snd_thread, &thread_return);
+    pthread_join(rcv_thread, &thread_return);
+    close(sock); // Close the socket
+    return 0;
 }
-	
-void * send_msg(void * arg)   // send thread main
-{
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
-	while(1) 
-	{
-		fgets(msg, BUF_SIZE, stdin);
-		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
-		{
-			close(sock);
-			exit(0);
-		}
-		sprintf(name_msg,"%s %s", name, msg);
-		write(sock, name_msg, strlen(name_msg));
-	}
-	return NULL;
+
+void * send_msg(void * arg) {
+    int sock = *((int *) arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+    while (1) {
+        fgets(msg, BUF_SIZE, stdin); // Read user input
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+            close(sock);
+            exit(0); // Exit program if user inputs "q" or "Q"
+        } else if (!strncmp(msg, "/invite ", 8) || !strcmp(msg, "/yes\n") || !strcmp(msg, "/no\n") || !strcmp(msg, "/out\n") || !strcmp(msg, "/id\n")) {
+            write(sock, msg, strlen(msg)); // Send special commands directly
+        } else {
+            sprintf(name_msg, "%s %s", name, msg); // Prepend username to the message
+            write(sock, name_msg, strlen(name_msg)); // Send the message
+        }
+    }
+    return NULL;
 }
-	
-void * recv_msg(void * arg)   // read thread main
-{
-	int sock=*((int*)arg);
-	char name_msg[NAME_SIZE+BUF_SIZE];
-	int str_len;
-	while(1)
-	{
-		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
-		if(str_len==-1) 
-			return (void*)-1;
-		name_msg[str_len]=0;
-		fputs(name_msg, stdout);
-	}
-	return NULL;
+
+void * recv_msg(void * arg) {
+    int sock = *((int *) arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+    int str_len;
+    while (1) {
+        str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1); // Read message from server
+        if (str_len == -1)
+            return (void *) -1;
+        name_msg[str_len] = 0; // Null-terminate the string
+        fputs(name_msg, stdout); // Print the message to the terminal
+    }
+    return NULL;
 }
-	
-void error_handling(char *msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
+
+void error_handling(char * msg) {
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1); // Print error message and exit the program
 }
